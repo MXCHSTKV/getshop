c6987f25f8fd3e9b4f49cddcff4ef630833b7ad3
Max Chistiakov <chistiakovmax@gmail.com>
2024-10-21 20:55:46 +0200

    Change styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '320px',
+        'md': '768px',
+        'lg': '1440px',
+      },
       colors: {
-
+        'white': '#FFFFFF',
+        'gray_01': '#A4ADAC',
+        'gray_02': '#D6DBDB',
+        'gray_03': '#E8EDEE',
+        'gray_04': '#F2F6F6',
+        'black': '#1D1D1B',
+        'orange': '#FF5900',
+        'dark_orange': '#E0570E',
+        'error': '#FF4026',
       },
     },
   },
