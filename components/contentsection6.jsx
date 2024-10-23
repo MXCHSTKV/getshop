62c5fa6119012a5247a90ee9180f9a00f0fd8d2b
Max Chistiakov <chistiakovmax@gmail.com>
2024-10-23 11:55:13 +0200

    Style bugs fixed

--- components/contentsection6.jsx
+++ components/contentsection6.jsx
@@ -2,7 +2,7 @@ import ValidateEmail from '@/components/validateemail.jsx'
 export default function ContentSection6() {
 
     return (
-        <section className="lg:w-[1440px] w-[768px] lg:h-[670px] h-[650px] lg:pt-[127px] bg-gray_03 flex items-center justify-between">  
+        <section className="lg:w-[1440px] w-[768px] lg:h-[670px] h-[650px] bg-gray_03 flex items-center justify-between">  
             <div className="flex flex-col justify-between lg:w-[1160px] w-[688px] lg:h-[333px] h-[550px] mx-auto">
                 <h2 className="font-bold text-[32px] line-h2">Заполните форму</h2>
                 <div className="lg:h-[216px] h-[484px] lg:w-[1160px] w-[688px] flex lg:flex-row flex-col justify-between">
--- components/main.jsx
+++ components/main.jsx
@@ -6,7 +6,7 @@ export default function ContentSectionOne() {
         <section>
             <Header />
             <main className="lg:w-[1220px] w-[688px] lg:h-[615px] h-[314px] lg:pt-[104px] lg:pl-[30px] mx-auto">
-                <h1 className="font-bold lg:text-[80px] text-[52px] text-left lg:mt-[100px] mt-[152px] lg:h-[184px] h-[114px] leading-tight lg:line-h1 line-h1-md tracking-[-2px] mx-auto">
+                <h1 className="font-bold lg:text-[80px] text-[52px] text-left lg:mt-[100px] mt-[152px] lg:h-[184px] h-[114px] leading-tight line-h1 tracking-[-2px] mx-auto">
                     Монетизируйте клиентскую<br />базу, не снижая NPS
                 </h1>
                 <p className="lg:text-[24px] text-[14px] font-regular block lg:w-[626px] w-[393px] lg:h-24 lg-[60px] mt-8 lg:mb-20 mb-[60px]">Найдите идеальный баланс выручки<br />и удовлетворённости пользователей с платформой рекламной монетизации</p>
