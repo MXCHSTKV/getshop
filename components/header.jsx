6ffdec87bdea94984b97ddb1f799b4de3e5643d9
Max Chistiakov <chistiakovmax@gmail.com>
2024-10-23 12:48:14 +0200

    Added <Menu />

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,12 @@
 "use client"
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
+import Menu from './menu'
 
 export default function Header() {
 const [isScrolled, setIsScrolled] = useState(false)
 const [width, setWidth] = useState(312)
+const [isActive, setIsActive] = useState(false)
 
 useEffect(() => {
     if (typeof window !== 'undefined') {   
@@ -28,7 +30,8 @@ useEffect(() => {
   const fixedHeader = isScrolled ? "shadow-[0_4px_8px_rgba(214,219,219,0.7)] bg-gray_03" : ""
 
     return (
-        <header className={`flex fixed h-[72px] lg:pl-[30px] px-[10px] lg:w-[1220px] w-[688px] justify-between items-center z-50 lg:ml-[110px] ml-[40px] ${fixedHeader}`}>
+        <header className={`flex fixed h-[72px] lg:pl-[30px] px-[10px] lg:w-[1220px] w-[688px] justify-between items-center z-20 lg:ml-[110px] ml-[40px] ${fixedHeader}`}>
+            {isActive ? <Menu /> : false}
             <Image
             src="/images/logo.svg"
             alt="Logo"
@@ -36,13 +39,23 @@ useEffect(() => {
             height={40}
             priority
             />
-            {width <= 1440 ? <Image
+            {width <= 1440 ? 
+            <button type='button' className='relative z-100' onClick={()=>{setIsActive(!isActive)}}>
+            {isActive ? <Image
+            src="/images/close.svg"
+            alt="Close"
+            width={44}
+            height={44}
+            priority
+            /> :
+            <Image
             src="/images/menu.svg"
             alt="Menu"
             width={44}
             height={44}
             priority
-            /> :
+            />}
+            </button>:
             <div className="w-[336px] h-[44px] flex justify-around items-center font-medium">
                 <a href="#advantages" className='hover:text-dark_orange '>Преимущества</a>
                 <a href="#work" className='hover:text-dark_orange '>Как работаем</a>
---
+++ components/menu.jsx
@@ -0,0 +1,10 @@
+export default function Menu() {
+    return (
+        <div className='fixed mt-[1060px] ml-[435px] w-[283px] h-[1130px] bg-white'>
+            <div className="flex flex-col justify-center items-start font-medium">
+                <a href="#advantages" className='hover:text-dark_orange mt-[526px] mb-[20px] ml-[32px] block'>Преимущества</a>
+                <a href="#work" className='hover:text-dark_orange ml-[32px] block'>Как работаем</a>
+            </div>
+        </div>
+    )
+}
